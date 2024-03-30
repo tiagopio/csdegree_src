1d83c682e6759fa5e5b7677bd2ac56f1827aa3c8
tiago <97364045+tiagopio@users.noreply.github.com>
2024-03-29 22:54:52 -0300

    Update ex22.c

--- IP/Lista-2/ex22.c
+++ IP/Lista-2/ex22.c
@@ -1,47 +1,39 @@
 #include <stdio.h>
+#include <math.h>
 
-void sep_digits(char *s, int *arr) {
-  int i;
-  for (i = 0; s[i] != '\0'; i++) {
-    arr[i] = s[i] - '0';
-  }
-}
-
-int solve() {
-  char user[100000];
-  int q, q_end, k, idx, max, c, v[100000];
-
-  while (1) {
-    scanf("%d %d%*c", &q, &q_end);
-    if (q == 0 && q_end == 0) break;
-    scanf("%s%*c", user);
-    sep_digits(user, v);
-
-    k = q - q_end;
-    max = 0;
-    for (c = 0; c <= k; c++) {
-      if (v[c] > max) {
-        max = v[c];
-        idx = c;
+void solve() {
+  int n, d;
+  
+  while(scanf("%d%d", &n, &d) && n + d) {
+    int v[n];
+    for (int i = 0;  i < n; i++) {
+      v[i] = -1;
+    }
+    int tmp = d;
+    int top = -1;
+    for (int i = 0; i < n; i++) {
+      int x; scanf("%1d", &x);
+      while(top > -1 && d && x > v[top]) {
+        top--;
+        d--;
       }
-
-      if (c == k) {
-        printf("%d", max);
-        max = 0;
-        k++;
-        c = idx;
-      }
-
-      if (k == q) break;
+      if (top + 1 < n - tmp) v[++top] = x;
+    }
+    for (int i = 0; i < n; i++) {
+      if (v[i] != -1) printf("%d", v[i]);
     }
     printf("\n");
   }
 
-  return 0;
 }
 
 int main() {
-  solve();
+  int t = 1;
+  
+  // scanf("%d%*c", &t);
+  while(t--) {
+    solve();
+  }
+
   return 0;
 }
-
