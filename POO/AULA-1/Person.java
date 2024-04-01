b4918dc38a5ae83ce6eb78a393f02679a63e537a
tiago <97364045+tiagopio@users.noreply.github.com>
2024-04-01 18:19:34 -0300

    Create Person.java

---
+++ POO/AULA-1/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+  public String name;
+  public Integer age;
+  public String sex;
+  
+  public Client(String name, Integer age, String email) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+}
