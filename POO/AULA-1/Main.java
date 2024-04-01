f2daca51d9a3ca9d6ad6471fda58686bb771cd9f
tiago <97364045+tiagopio@users.noreply.github.com>
2024-04-01 18:19:56 -0300

    Update Main.java

--- POO/AULA-1/Main.java
+++ POO/AULA-1/Main.java
@@ -2,7 +2,7 @@ public class Main {
 	public static void main(String args[]) {
 		Person person = new Person("Tiago", 19, "M");
 		System.out.println(person.name);
-    System.out.println(person.age);
-    System.out.println(person.sex); 
+  	System.out.println(person.age);
+  	System.out.println(person.sex); 
 	}
 }
