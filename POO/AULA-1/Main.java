4d62ce21ad5c7fbba28934d455d16968730607f0
tiago <97364045+tiagopio@users.noreply.github.com>
2024-04-01 18:18:47 -0300

    Create Main.java

---
+++ POO/AULA-1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String args[]) {
+		Person person = new Person("Tiago", 19, "M");
+		System.out.println(person.name);
+    System.out.println(person.age);
+    System.out.println(person.sex); 
+	}
+}
